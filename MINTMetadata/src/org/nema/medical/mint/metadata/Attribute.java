cca670f354f1638abac073cd59a1d496643e1ae9
tim.dawson <tim.dawson@gmail.com>
2010-07-06 19:59:02 +0000

    separating metadata from common

---
+++ MINTMetadata/src/org/nema/medical/mint/metadata/Attribute.java
@@ -0,0 +1,210 @@
+/*
+ *   Copyright 2010 MINT Working Group
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.nema.medical.mint.metadata;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.AttributeData;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.ItemData;
+
+
+/**
+ * Schema fragment(s) for this class:
+ * <pre>
+ * &lt;xs:complexType xmlns:ns="http://medical.nema.org/mint" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="AttrType">
+ *   &lt;xs:sequence>
+ *     &lt;xs:element type="ns:ItemType" name="Item" minOccurs="0" maxOccurs="1"/>
+ *   &lt;/xs:sequence>
+ *   &lt;xs:attribute type="xs:string" use="required" name="tag"/>
+ *   &lt;xs:attribute type="xs:string" use="required" name="vr"/>
+ *   &lt;xs:attribute type="xs:string" use="optional" name="val"/>
+ *   &lt;xs:attribute type="xs:string" use="optional" name="bid"/>
+ * &lt;/xs:complexType>
+ * </pre>
+ */
+public class Attribute
+{
+    private final List<Item> items = new ArrayList<Item>();
+    private int tag;
+    private String vr;
+    private String val;
+    private int bid = -1; // index must be a positive integer
+    private String exclude;
+
+    /**
+     * puts an Item into the Attribute
+     * @param attr
+     */
+    public void addItem(final Item item) {
+        items.add(item);
+    }
+
+    /**
+     * removes the Item at the given index in the list
+     * @param tag
+     */
+    public void removeItem(final int index) {
+        items.remove(index);
+    }
+
+    /**
+     * @return an iterator of all Items in the Attribute
+     */
+    public Iterator<Item> itemIterator() {
+        return items.iterator();
+    }
+
+    /**
+     * @return true if the Attribute has sequence items
+     */
+    public boolean hasSequenceItems() {
+        return !items.isEmpty();
+    }
+
+    /**
+     * Get the 'tag' attribute value.
+     *
+     * @return value
+     */
+    public int getTag() {
+        return tag;
+    }
+
+    /**
+     * Set the 'tag' attribute value.
+     *
+     * @param tag
+     */
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * Set the 'tag' attribute value ... as a hex string
+     *
+     * @param tag
+     */
+    public void setTag(String hex) {
+        this.tag = StudyIO.hex2int(hex);
+    }
+
+    /**
+     * Get the 'vr' attribute value.
+     *
+     * @return value
+     */
+    public String getVr() {
+        return vr;
+    }
+
+    /**
+     * Set the 'vr' attribute value.
+     *
+     * @param vr
+     */
+    public void setVr(String vr) {
+        this.vr = vr;
+    }
+
+    /**
+     * Get the 'val' attribute value.
+     *
+     * @return value
+     */
+    public String getVal() {
+        return val;
+    }
+
+    /**
+     * Set the 'val' attribute value.
+     *
+     * @param val
+     */
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    /**
+     * Get the 'bid' attribute value.
+     *
+     * @return value
+     */
+    public int getBid() {
+        return bid;
+    }
+
+    /**
+     * Set the 'bid' attribute value.
+     * Negative values are not valid, except -1 which signifies no value,
+     * since zero is valid and null is not allowed for a primitive.
+     * @param bid
+     */
+    public void setBid(int bid) {
+    	if (bid < -1) bid = -1;
+        this.bid = bid;
+    }
+
+    /**
+     * Get the 'exclude' attribute value.
+     *
+     * @return value
+     */
+    public String getExclude() {
+		return exclude;
+	}
+
+    /**
+     * Set the 'exclude' attribute value.
+     *
+     * @param exclude
+     */
+	public void setExclude(String exclude) {
+		this.exclude = exclude;
+	}
+
+    //
+    //  Google Protocol Buffer support - package protection intentional
+    //
+    static Attribute fromGPB(AttributeData attrData) {
+        Attribute attr = new Attribute();
+        attr.setTag(attrData.getTag());
+        attr.setVr(attrData.getVr());
+        if (attrData.hasStringValue()) attr.setVal(attrData.getStringValue());
+        if (attrData.hasExclude()) attr.setExclude(attrData.getStringValue());
+        if (attrData.hasBinaryItemId()) { attr.setBid(attrData.getBinaryItemId()); }
+        for (ItemData itemData : attrData.getItemsList()) {
+            attr.addItem(Item.fromGPB(itemData));
+        }
+        return attr;
+    }
+
+    AttributeData toGPB() {
+        AttributeData.Builder builder = AttributeData.newBuilder();
+        builder.setTag(this.tag);
+        if (this.bid >=0 ) builder.setBinaryItemId(this.bid);
+        if (this.vr != null) builder.setVr(this.vr);
+        if (this.val != null) builder.setStringValue(this.val);
+        if (this.exclude != null) builder.setExclude(this.val);
+        for (Item item : this.items) {
+            builder.addItems(item.toGPB());
+        }
+        AttributeData data = builder.build();
+        return data;
+    }
+}
---
+++ MINTMetadata/src/org/nema/medical/mint/metadata/StudyIO.java
@@ -0,0 +1,192 @@
+/*
+ *   Copyright 2010 MINT Working Group
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package org.nema.medical.mint.metadata;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.jibx.runtime.BindingDirectory;
+import org.jibx.runtime.IBindingFactory;
+import org.jibx.runtime.IMarshallingContext;
+import org.jibx.runtime.IUnmarshallingContext;
+import org.jibx.runtime.JiBXException;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.StudyData;
+
+import com.google.gson.Gson;
+
+public class StudyIO {
+	
+	static public Study parseFromXML(File file) throws IOException {
+		Study study = null;
+		InputStream in = new FileInputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			in = new GZIPInputStream(in);
+		}
+		try {
+			study = parseFromXML(in);
+		} finally {
+			in.close();
+		}
+		return study;
+	}
+	
+	static public Study parseFromXML(InputStream in) throws IOException {
+		Study study = null;
+		try {
+			IBindingFactory bfact = BindingDirectory.getFactory(Study.class);
+			IUnmarshallingContext uctx = bfact.createUnmarshallingContext();
+			study = (Study)uctx.unmarshalDocument(in, null);
+		} catch (JiBXException e) {
+			throw new IOException("Exception while unmarshalling data.",e);
+		}
+		return study;
+	}
+	
+	static public void writeToXML(Study study, File file) throws IOException {
+		OutputStream out = new FileOutputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			out = new GZIPOutputStream(out);
+		}
+		try {
+			writeToXML(study, out);
+		} finally {
+			out.close();
+		}		
+	}
+	
+	static public void writeToXML(Study study, OutputStream out) throws IOException {
+		try {
+			IBindingFactory bfact = BindingDirectory.getFactory(Study.class);
+			IMarshallingContext mctx = bfact.createMarshallingContext();
+			mctx.setIndent(2);
+			mctx.marshalDocument(study, "UTF-8", null, out);
+		} catch (JiBXException e) {
+			throw new IOException("Exception while marshalling data.",e);
+		}		
+	}
+	
+	static public Study parseFromJSON(File file) throws IOException {
+		Study study = null;
+		InputStream in = new FileInputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			in = new GZIPInputStream(in);
+		}
+		try {
+			study = parseFromJSON(in);
+		} finally {
+			in.close();
+		}
+		return study;
+	}
+	
+	static public Study parseFromJSON(InputStream in) {
+		Reader reader = new InputStreamReader(in);
+		Study study = new Gson().fromJson(reader, Study.class);
+		return study;
+	}
+	
+	static public void writeToJSON(Study study, File file) throws IOException {
+		OutputStream out = new FileOutputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			out = new GZIPOutputStream(out);
+		}
+		try {
+			writeToJSON(study, out);
+		} finally {
+			out.close();
+		}				
+	}	
+	
+	static public void writeToJSON(Study study, OutputStream out) throws IOException {
+		Writer writer = new OutputStreamWriter(out);
+		writer.write(new Gson().toJson(study));
+		writer.flush();
+	}	
+
+	static public Study parseFromGPB(File file) throws IOException {
+		Study study = null;
+		InputStream in = new FileInputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			in = new GZIPInputStream(in);
+		}
+		try {
+			study = parseFromGPB(in);
+		} finally {
+			in.close();
+		}
+		return study;
+	}
+	
+	static public Study parseFromGPB(InputStream in) throws IOException {
+		StudyData data = StudyData.parseFrom(in);		
+		return Study.fromGPB(data);		
+	}
+	
+	static public void writeToGPB(Study study, File file) throws IOException {
+		OutputStream out = new FileOutputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			out = new GZIPOutputStream(out);
+		}
+		try {
+			writeToGPB(study, out);
+		} finally {
+			out.close();
+		}		
+	}
+	
+	static public void writeToGPB(Study study, OutputStream out) throws IOException {
+		StudyData data = study.toGPB();
+		data.writeTo(out);
+	}
+
+    // used to convert Attribute.tag int to hex
+	static public String int2hex(int tag) {
+        return String.format("%08x", tag);
+    }
+
+    // used to convert Attribute.tag hex to int
+	static public int hex2int(String hex) {
+        if (hex.length()>8) throw new NumberFormatException("max value is 32 bytes (unsigned)");
+        return (int)Long.parseLong(hex,16);
+    }
+
+    // used to convert Attribute.tag int to hex
+	static public String int2bid(int tag) {
+        return (tag >= 0) ? String.format("%d", tag) : "";
+    }
+
+    // used to convert Attribute.tag hex to int
+	static public int bid2int(String bid) {
+		if (bid == null || bid.length() == 0) {
+			return -1;
+		} else {
+	        return Integer.parseInt(bid);		
+		}
+    }
+
+}
