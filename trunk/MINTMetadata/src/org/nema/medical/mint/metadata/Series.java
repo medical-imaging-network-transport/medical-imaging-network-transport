288a7253c0db69dcb96f6dd6ed3e9afc2c132267
tim.dawson <tim.dawson@ef132cb6-371b-4f86-0ab4-8a9baf3c28dc>
2010-07-06 19:59:02 +0000

    separating metadata from common

---
+++ trunk/MINTMetadata/src/org/nema/medical/mint/metadata/Series.java
@@ -0,0 +1,206 @@
+/*
+ *   Copyright 2010 MINT Working Group
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.nema.medical.mint.metadata;
+
+import java.util.*;
+
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.AttributeData;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.InstanceData;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.SeriesData;
+
+
+/**
+ * Schema fragment(s) for this class:
+ * <pre>
+ * &lt;xs:complexType xmlns:ns="http://medical.nema.org/mint" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="SeriesType">
+ *   &lt;xs:sequence>
+ *     &lt;xs:element type="ns:AttributesType" name="Attributes" minOccurs="1" maxOccurs="1"/>
+ *     &lt;xs:element type="ns:NormalizedInstanceAttributesType" name="NormalizedInstanceAttributes" minOccurs="1" maxOccurs="1"/>
+ *     &lt;xs:element type="ns:InstancesType" name="Instances" minOccurs="1" maxOccurs="1"/>
+ *   &lt;/xs:sequence>
+ *   &lt;xs:attribute type="xs:string" use="required" name="seriesInstanceUID"/>
+ * &lt;/xs:complexType>
+ * </pre>
+ */
+public class Series implements AttributeStore
+{
+    private final Map<Integer,Attribute> attributeMap = new HashMap<Integer,Attribute>();
+    private final Map<Integer,Attribute> normalizedInstanceAttributeMap = new HashMap<Integer,Attribute>();
+    private final List<Instance> instanceList = new ArrayList<Instance>();
+    private String seriesInstanceUID;
+    private String exclude;
+
+    /**
+     * @param tag
+     * @return the attribute for the given tag
+     */
+    public Attribute getAttribute(final int tag) {
+        return attributeMap.get(tag);
+    }
+
+    /**
+     * puts an Attribute into the Series - attributes are unique per tag
+     * @param attr
+     */
+    public void putAttribute(final Attribute attr) {
+        attributeMap.put(attr.getTag(), attr);
+    }
+
+    /**
+     * removes the Attribute with the given tag from the Series
+     * @param tag
+     */
+    public void removeAttribute(final int tag) {
+        attributeMap.remove(tag);
+    }
+
+    /**
+     * @return an iterator of all Attributes in the Series
+     */
+    public Iterator<Attribute> attributeIterator() {
+        return attributeMap.values().iterator();
+    }
+
+    /**
+     * @param tag
+     * @return the normalized instance attribute for the given tag
+     */
+    public Attribute getNormalizedInstanceAttribute(final String tag) {
+        return normalizedInstanceAttributeMap.get(tag);
+    }
+
+    /**
+     * puts a NormalizedInstanceAttribute into the Series - attributes are unique per tag
+     * @param attr
+     */
+    public void putNormalizedInstanceAttribute(final Attribute attr) {
+        normalizedInstanceAttributeMap.put(attr.getTag(), attr);
+    }
+
+    /**
+     * removes the NormalizedInstanceAttribute with the given tag from the Series
+     * @param tag
+     */
+    public void removeNormalizedInstanceAttribute(final String tag) {
+        normalizedInstanceAttributeMap.remove(tag);
+    }
+
+    /**
+     * @return an iterator of all NormalizedInstanceAttributes in the Series
+     */
+    public Iterator<Attribute> normalizedInstanceAttributeIterator() {
+        return normalizedInstanceAttributeMap.values().iterator();
+    }
+
+    /**
+     * puts an Instance into the Series - instances are unique per xfer
+     * @param inst
+     */
+    public void putInstance(final Instance inst) {
+        instanceList.add(inst);
+    }
+
+    /**
+     * @return an iterator of all Instances in the Series
+     */
+    public Iterator<Instance> instanceIterator() {
+        return instanceList.iterator();
+    }
+
+    /**
+     * @return the number of Instances in the Series
+     */
+    public int instanceCount() {
+        return instanceList.size();
+    }
+
+    /**
+     * Get the 'seriesInstanceUID' attribute value.
+     *
+     * @return value
+     */
+    public String getSeriesInstanceUID() {
+        return seriesInstanceUID;
+    }
+
+    /**
+     * Set the 'seriesInstanceUID' attribute value.
+     *
+     * @param seriesInstanceUID
+     */
+    public void setSeriesInstanceUID(String seriesInstanceUID) {
+        this.seriesInstanceUID = seriesInstanceUID;
+    }
+
+    /**
+     * Get the 'exclude' attribute value.
+     *
+     * @return value
+     */
+    public String getExclude() {
+		return exclude;
+	}
+
+    /**
+     * Set the 'exclude' attribute value.
+     *
+     * @param exclude
+     */
+	public void setExclude(String exclude) {
+		this.exclude = exclude;
+	}
+
+	//
+    // Google Protocol Buffer support - package protection intentional
+    //
+    static Series fromGPB(SeriesData data) {
+        Series series = new Series();
+        series.setSeriesInstanceUID(data.getSeriesInstanceUid());
+        if (data.hasExclude()) series.setExclude(data.getExclude());
+        for (AttributeData attrData : data.getAttributesList()) {
+            series.putAttribute(Attribute.fromGPB(attrData));
+        }
+        for (AttributeData attrData : data.getNormalizedInstanceAttributesList()) {
+            series.putNormalizedInstanceAttribute(Attribute.fromGPB(attrData));
+        }
+        for (InstanceData instData : data.getInstancesList()) {
+            series.putInstance(Instance.fromGPB(instData));
+        }
+        return series;
+    }
+
+    SeriesData toGPB() {
+        SeriesData.Builder builder = SeriesData.newBuilder();
+        if (this.seriesInstanceUID != null ) {
+        	builder.setSeriesInstanceUid(this.seriesInstanceUID);
+        }
+        if (this.exclude != null) {
+        	builder.setExclude(this.exclude);
+        }
+        for (Attribute attr : this.attributeMap.values()) {
+            builder.addAttributes(attr.toGPB());
+        }
+        for (Attribute attr : this.normalizedInstanceAttributeMap.values()) {
+            builder.addNormalizedInstanceAttributes(attr.toGPB());
+        }
+        for (Instance inst : this.instanceList) {
+            builder.addInstances(inst.toGPB());
+        }
+        SeriesData data = builder.build();
+        return data;
+    }
+}
---
+++ trunk/MINTMetadata/src/org/nema/medical/mint/metadata/Study.java
@@ -0,0 +1,163 @@
+/*
+ *   Copyright 2010 MINT Working Group
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.nema.medical.mint.metadata;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.AttributeData;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.SeriesData;
+import org.nema.medical.mint.metadata.gpb.MINT2GPB.StudyData;
+
+
+/**
+ * Schema fragment(s) for this class:
+ * <pre>
+ * &lt;xs:complexType xmlns:ns="http://medical.nema.org/mint" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="StudyMetaType">
+ *   &lt;xs:sequence>
+ *     &lt;xs:element type="ns:AttributesType" name="Attributes" minOccurs="1" maxOccurs="1"/>
+ *     &lt;xs:element type="ns:SeriesListType" name="SeriesList"/>
+ *   &lt;/xs:sequence>
+ *   &lt;xs:attribute type="xs:string" use="required" name="studyInstanceUID"/>
+ * &lt;/xs:complexType>
+ * </pre>
+ */
+public class Study implements AttributeStore
+{
+    private final Map<Integer,Attribute> attributeMap = new HashMap<Integer,Attribute>();
+    private final Map<String,Series> seriesMap = new HashMap<String,Series>();
+    private String studyInstanceUID;
+
+    /**
+     * @param tag
+     * @return the attribute for the given tag
+     */
+    public Attribute getAttribute(final int tag) {
+        return attributeMap.get(tag);
+    }
+
+    public String getValueForAttribute(final int tag) {
+        Attribute attr = getAttribute(tag);
+        return attr != null ? attr.getVal() : null;
+    }
+
+    /**
+     * puts an Attribute into the Series - attributes are unique per tag
+     * @param attr
+     */
+    public void putAttribute(final Attribute attr) {
+        attributeMap.put(attr.getTag(), attr);
+    }
+
+    /**
+     * removes the Attribute with the given tag from the Series
+     * @param tag
+     */
+    public void removeAttribute(final int tag) {
+        attributeMap.remove(tag);
+    }
+
+    /**
+     * @return an iterator of all Attributes in the Series
+     */
+    public Iterator<Attribute> attributeIterator() {
+        return attributeMap.values().iterator();
+    }
+
+    /**
+     * @param uid
+     * @return the series for the given uid
+     */
+    public Series getSeries(final String uid) {
+        return seriesMap.get(uid);
+    }
+
+    /**
+     * puts an Series into the Study - series are unique per uid
+     * @param attr
+     */
+    public void putSeries(final Series series) {
+        seriesMap.put(series.getSeriesInstanceUID(), series);
+    }
+
+    /**
+     * removes the Series with the given uid from the Study
+     * @param uid
+     */
+    public void removeSeries(final String uid) {
+        seriesMap.remove(uid);
+    }
+
+    /**
+     * @return an iterator of all Series in the Study
+     */
+    public Iterator<Series> seriesIterator() {
+        return seriesMap.values().iterator();
+    }
+
+    /**
+     * Get the 'studyInstanceUID' attribute value.
+     *
+     * @return value
+     */
+    public String getStudyInstanceUID() {
+        return studyInstanceUID;
+    }
+
+    /**
+     * Set the 'studyInstanceUID' attribute value.
+     *
+     * @param studyInstanceUID
+     */
+    public void setStudyInstanceUID(String studyInstanceUID) {
+        this.studyInstanceUID = studyInstanceUID;
+    }
+
+    //  Google Protocol Buffer support - package protection intentional
+    //  Google Protocol Buffer support
+    //
+    static Study fromGPB(StudyData studyData) {
+        Study study = new Study();
+        study.setStudyInstanceUID(studyData.getStudyInstanceUid());
+        for (AttributeData attrData : studyData.getAttributesList()) {
+            Attribute attr = Attribute.fromGPB(attrData);
+            study.putAttribute(attr);
+        }
+        for (SeriesData seriesData : studyData.getSeriesList()) {
+            Series series = Series.fromGPB(seriesData);
+            study.putSeries(series);
+        }
+        return study;
+    }
+
+    StudyData toGPB() {
+        StudyData.Builder builder = StudyData.newBuilder();
+        if (this.studyInstanceUID != null) {
+        	builder.setStudyInstanceUid(this.studyInstanceUID);
+        }
+        for (Attribute attr : this.attributeMap.values()) {
+            builder.addAttributes(attr.toGPB());
+        }
+        for (Series series: this.seriesMap.values()) {
+            builder.addSeries(series.toGPB());
+        }
+        StudyData data = builder.build();
+        return data;
+    }
+
+}
