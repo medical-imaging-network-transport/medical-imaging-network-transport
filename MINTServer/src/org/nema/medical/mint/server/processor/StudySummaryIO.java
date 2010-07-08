8df96dbd04493706cbfd43f496d46806cf8aa08a
ububen <ububen@gmail.com>
2010-07-08 17:11:18 +0000

    Fix file paths.

--- MINTServer/src/org/nema/medical/mint/server/processor/StudySummaryIO.java
+++ MINTServer/src/org/nema/medical/mint/server/processor/StudySummaryIO.java
@@ -31,81 +31,81 @@ import org.nema.medical.mint.metadata.StudyIO;
 
 public class StudySummaryIO {
 
-	static private final String VELOCITY_PROPERTIES = "velocity.properties";
-	static private final String STUDY_ATTRIBUTES = "org/nema/medical/mint/common/metadata/StudySummary.properties";
-	static private final String SUMMARY_TEMPLATE = "org/nema/medical/mint/common/metadata/StudySummary.vm";
-	static private Properties summaryAttrProps;
-	static private Properties velocityProps;
+    static private final String VELOCITY_PROPERTIES = "velocity.properties";
+    static private final String STUDY_ATTRIBUTES = "org/nema/medical/mint/server/processor/StudySummary.properties";
+    static private final String SUMMARY_TEMPLATE = "org/nema/medical/mint/server/processor/StudySummary.vm";
+    static private Properties summaryAttrProps;
+    static private Properties velocityProps;
 
-	static private Properties getSummaryAttrProps() throws IOException {
-		if (summaryAttrProps == null)
-			synchronized (StudySummaryIO.class) {
-				if (summaryAttrProps == null) {
-					summaryAttrProps = getPropertiesFromResource(STUDY_ATTRIBUTES);
-				}
-			}
-		return summaryAttrProps;
-	}
+    static private Properties getSummaryAttrProps() throws IOException {
+        if (summaryAttrProps == null)
+            synchronized (StudySummaryIO.class) {
+                if (summaryAttrProps == null) {
+                    summaryAttrProps = getPropertiesFromResource(STUDY_ATTRIBUTES);
+                }
+            }
+        return summaryAttrProps;
+    }
 
-	static private Properties getVelocityProperties() throws IOException {
-		if (velocityProps == null)
-			synchronized (StudySummaryIO.class) {
-				if (velocityProps == null) {
-					velocityProps = getPropertiesFromResource(VELOCITY_PROPERTIES);
-				}
-			}
-		return velocityProps;
-	}
+    static private Properties getVelocityProperties() throws IOException {
+        if (velocityProps == null)
+            synchronized (StudySummaryIO.class) {
+                if (velocityProps == null) {
+                    velocityProps = getPropertiesFromResource(VELOCITY_PROPERTIES);
+                }
+            }
+        return velocityProps;
+    }
 
-	private static Properties getPropertiesFromResource(String resourceName)
-			throws IOException {
-		Properties results = new Properties();
-		InputStream stream = null;
-		try {
-			stream = StudySummaryIO.class.getClassLoader().getResourceAsStream(resourceName);
-			results.load(stream);
-		} finally {
-			if (stream != null) stream.close();
-		}
-		return results;
-	}
+    private static Properties getPropertiesFromResource(String resourceName)
+            throws IOException {
+        Properties results = new Properties();
+        InputStream stream = null;
+        try {
+            stream = StudySummaryIO.class.getClassLoader().getResourceAsStream(resourceName);
+            results.load(stream);
+        } finally {
+            if (stream != null) stream.close();
+        }
+        return results;
+    }
 
-	static public void writeSummaryToXHTML(Study study, File file)
-			throws IOException {
-		FileWriter writer = new FileWriter(file);
-		try {
-			StudySummaryIO.writeSummaryToXHTML(study, writer);
-		} finally {
-			writer.close();
-		}
-	}
+    static public void writeSummaryToXHTML(Study study, File file)
+            throws IOException {
+        FileWriter writer = new FileWriter(file);
+        try {
+            StudySummaryIO.writeSummaryToXHTML(study, writer);
+        } finally {
+            writer.close();
+        }
+    }
 
-	static public void writeSummaryToXHTML(Study study, Writer writer)
-			throws IOException {
-		Properties descriptions = StudySummaryIO.getSummaryAttrProps();
-		Properties values = new Properties();
-		try {
-			VelocityEngine ve = new VelocityEngine();
-			ve.init(getVelocityProperties());
-			Template t = ve.getTemplate(SUMMARY_TEMPLATE);
-			VelocityContext context = new VelocityContext();
+    static public void writeSummaryToXHTML(Study study, Writer writer)
+            throws IOException {
+        Properties descriptions = StudySummaryIO.getSummaryAttrProps();
+        Properties values = new Properties();
+        try {
+            VelocityEngine ve = new VelocityEngine();
+            ve.init(getVelocityProperties());
+            Template t = ve.getTemplate(SUMMARY_TEMPLATE);
+            VelocityContext context = new VelocityContext();
 
-			// put all descriptions, and the available values in the context
-			context.put("descriptions", descriptions);
-			for (Object key : descriptions.keySet()) {
-				Attribute attr = study.getAttribute(StudyIO.hex2int(key
-						.toString()));
-				if (attr != null && attr.getVal() != null) {
-					values.put(key, attr.getVal());
-				}
-			}
-			context.put("values", values);
-			context.put("studyInstanceUID", study.getStudyInstanceUID());
+            // put all descriptions, and the available values in the context
+            context.put("descriptions", descriptions);
+            for (Object key : descriptions.keySet()) {
+                Attribute attr = study.getAttribute(StudyIO.hex2int(key
+                        .toString()));
+                if (attr != null && attr.getVal() != null) {
+                    values.put(key, attr.getVal());
+                }
+            }
+            context.put("values", values);
+            context.put("studyInstanceUID", study.getStudyInstanceUID());
 
-			/* now render the template */
-			t.merge(context, writer);
-		} catch (Exception e) {
-			throw new RuntimeException("unable to write summary", e);
-		}
-	}
+            /* now render the template */
+            t.merge(context, writer);
+        } catch (Exception e) {
+            throw new RuntimeException("unable to write summary", e);
+        }
+    }
 }
