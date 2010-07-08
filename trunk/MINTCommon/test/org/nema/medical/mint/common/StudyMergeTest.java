81c02341bfe67108598b833303c3818edd159cfb
tim.dawson <tim.dawson@ef132cb6-371b-4f86-0ab4-8a9baf3c28dc>
2010-07-08 14:26:41 +0000

    added junit and sample test to MINTCommon; minor cleanup to MINTMetadata build file for consistency

---
+++ trunk/MINTCommon/test/org/nema/medical/mint/common/StudyMergeTest.java
@@ -0,0 +1,18 @@
+package org.nema.medical.mint.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class StudyMergeTest {
+
+	@Test
+	public void testNothing() throws Exception {
+		// simple test to provide junit infrastructure - replace this
+		// when study merge functionality exists
+		assertEquals(0,0);
+		assertNotNull("stupid test");
+	}
+
+}
