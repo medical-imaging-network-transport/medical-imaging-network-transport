013ad4eb85114bc22b5d6009e0b4ae2a3186df79
tim.dawson <tim.dawson@gmail.com>
2010-07-08 13:34:14 +0000

    fixed upload error, stray creation of $MINT_HOME in tomcat directory, moved some files out of common that belong in server now

--- MINTServer/src/org/nema/medical/mint/server/processor/StudySummaryIO.java
+++ MINTServer/src/org/nema/medical/mint/server/processor/StudySummaryIO.java
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package org.nema.medical.mint.common.metadata;
+package org.nema.medical.mint.server.processor;
 
 import java.io.File;
 import java.io.FileWriter;
