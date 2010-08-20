e431ce9b99adb319098e48e26b2c8b88893eb91b
tim.dawson@gmail.com <tim.dawson@gmail.com>
2010-08-20 17:13:01 +0000

    removed JSON

--- MINTMetadata/src/org/nema/medical/mint/metadata/Instance.java
+++ MINTMetadata/src/org/nema/medical/mint/metadata/Instance.java
@@ -20,8 +20,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.nema.medical.mint.metadata.gpb.MINT2GPB.AttributeData;
-import org.nema.medical.mint.metadata.gpb.MINT2GPB.InstanceData;
+import org.nema.medical.mint.metadata.GPB.AttributeData;
+import org.nema.medical.mint.metadata.GPB.InstanceData;
 
 
 /**
