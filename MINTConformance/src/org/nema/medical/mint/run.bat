625813c5598097ea33a99456847bfbe99435fcc0
softtrac <softtrac@gmail.com>
2010-09-01 19:36:22 +0000

    Updated MintDicomCompare application to compare studies with sequencing data elements.

--- MINTConformance/src/org/nema/medical/mint/run.bat
+++ MINTConformance/src/org/nema/medical/mint/run.bat
@@ -15,8 +15,8 @@ set PYTHONPATH=%MC_HOME%\MINTConformance\src
 echo "Parse a DICOM Instance..."
 %PYTHON% %MC_HOME%\%MC_SRC%\DicomInstance.py %MC_HOME%\%MC_DICOM%\1.2.392.200036.9116.2.2.2.1762660474.1026398161.357037.dcm
 
-REM echo "Parse a DICOM Instance (with sequences)..."
-REM %PYTHON% %MC_HOME%\%MC_SRC%\DicomInstance.py -e %MC_HOME%\%MC_DICOM_MINT_SQ%\2.16.840.1.114255.187086712.1704407169.30043.44.00004461237.dcm
+echo "Parse a DICOM Instance (with sequences)..."
+%PYTHON% %MC_HOME%\%MC_SRC%\DicomInstance.py %MC_HOME%\%MC_DICOM_MINT_SQ%\2.16.840.1.114255.187086712.1704407169.30043.44.00004461237.dcm
 
 echo "Parse a DICOM Study..."
 %PYTHON% %MC_HOME%\%MC_SRC%\DicomStudy.py %MC_HOME%\%MC_DICOM%
@@ -35,3 +35,6 @@ echo "Compare MINT Studies (with sequences)..."
 
 echo "Compare DICOM to MINT..."
 %PYTHON% %MC_HOME%\%MC_SRC%\MintDicomCompare.py -v %MC_HOME%\%MC_DICOM% %MC_HOME%\%MC_MINT%\metadata.xml
+
+echo "Compare DICOM to MINT (with sequences)..."
+%PYTHON% %MC_HOME%\%MC_SRC%\MintDicomCompare.py -v %MC_HOME%\%MC_DICOM_MINT_SQ% %MC_HOME%\%MC_DICOM_MINT_SQ%\metadata.xml
