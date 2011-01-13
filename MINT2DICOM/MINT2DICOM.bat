abf5479f130a69859b1a97e8766588bc317081c7
ububen@gmail.com <ububen@gmail.com>
2011-01-13 18:21:32 +0000

    Adjusting data dictionary JIBX definition to reflect XSD Incrementing build number for next official MINT build

--- MINT2DICOM/MINT2DICOM.bat
+++ MINT2DICOM/MINT2DICOM.bat
@@ -6,7 +6,7 @@ set USE_BULK=%4
 
 if {%4}=={} goto USAGE
 
-set CLASSPATH="build\MINT2DICOM.jar;..\MINTServer\WebContent\WEB-INF\lib\dcm4che-core-2.0.23.jar;..\MINTJavaSDK\build\mint-0.9.13.jar;..\MINTJavaSDK\lib\jibx-run-1.2.2.jar;..\MINTJavaSDK\lib\protobuf-java-2.3.0.jar;..\MINTServer\WebContent\WEB-INF\lib\slf4j-api-1.6.0.jar;..\MINTServer\WebContent\WEB-INF\lib\slf4j-log4j12-1.6.0.jar;..\MINTServer\WebContent\WEB-INF\lib\log4j-1.2.16.jar;"
+set CLASSPATH="build\MINT2DICOM.jar;..\MINTServer\WebContent\WEB-INF\lib\dcm4che-core-2.0.23.jar;..\MINTJavaSDK\build\mint-0.9.14.jar;..\MINTJavaSDK\lib\jibx-run-1.2.2.jar;..\MINTJavaSDK\lib\protobuf-java-2.3.0.jar;..\MINTServer\WebContent\WEB-INF\lib\slf4j-api-1.6.0.jar;..\MINTServer\WebContent\WEB-INF\lib\slf4j-log4j12-1.6.0.jar;..\MINTServer\WebContent\WEB-INF\lib\log4j-1.2.16.jar;"
 @java -Xms32m -Xmx512m -cp %CLASSPATH% org.nema.medical.mint.MINT2DICOM http://%HOSTNAME%:8080/MINTServer/studies/%STUDY%/DICOM/ %OUTDIR% %USE_BULK%
 GOTO:EOF
 
